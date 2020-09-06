1df749314ae97dc6949d711778e7fbb212cb80a6
Anton Johansson <anton.elis@hotmail.com>
2020-09-06 16:33:44 +0200

    Implemented assembler for hyperlink, REST-api

--- Backend/Service/src/main/java/se/iths/service/StudentsController.java
+++ Backend/Service/src/main/java/se/iths/service/StudentsController.java
@@ -1,5 +1,8 @@
 package se.iths.service;
+
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,38 +10,43 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentsController {
 
     final StudentsRepository repository;
+    private final StudentsModelAssembler assembler;
 
-    public StudentsController(StudentsRepository storage) {
+    public StudentsController(StudentsRepository storage, StudentsModelAssembler studentsModelAssembler) {
         this.repository = storage;
+        this.assembler = studentsModelAssembler;
     }
 
     @GetMapping()
-    public List<Student> allStudents() {
+    public CollectionModel<EntityModel<Student>> allStudents() {
         log.info("All Students called");
-        return repository.findAll();
+        return assembler.toCollectionModel(repository.findAll());
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Student> oneStudent(@PathVariable Integer id) {
-        var studentOptional = repository.findById(id);
-        return studentOptional.map(student -> new ResponseEntity<>(student, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    public ResponseEntity<EntityModel<Student>> oneStudent(@PathVariable Integer id) {
+        return repository.findById(id)
+                .map(assembler::toModel)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping()
     public ResponseEntity<?> createStudent(@RequestBody Student student) {
         log.info("POST create Person " + student);
-        var s =repository.save(student);
+        var s = repository.save(student);
         log.info("Saved to repository " + s);
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Location", "/api/v1/students/" + student.getId());
-        return new ResponseEntity<>(s, headers, HttpStatus.CREATED);
+        headers.setLocation(linkTo(StudentsController.class).slash(s.getId()).toUri());
+        return new ResponseEntity<>(assembler.toModel(s), headers, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
@@ -52,16 +60,24 @@ public class StudentsController {
     }
 
     @PutMapping("/{id}")
-    ResponseEntity<Student> replaceStudent(@RequestBody Student newStudent, @PathVariable Integer id) {
-        return repository.findById(id).map(student -> {
-            student.setFirstName(newStudent.getFirstName());
-            student.setLastName(newStudent.getLastName());
-            student.setEducation(newStudent.getEducation());
-            repository.save(student);
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Location", "/api/v1/students/" + student.getId());
-            return new ResponseEntity<>(student, headers, HttpStatus.OK);
-        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    ResponseEntity<EntityModel<Student>> replaceStudent(@RequestBody Student newStudent, @PathVariable Integer id) {
+        if (repository.findById(id).isPresent()) {
+            log.info("IF");
+            var p = repository.findById(id)
+                    .map(existingUser -> {
+                        existingUser.setFirstName(newStudent.getFirstName());
+                        existingUser.setLastName(newStudent.getLastName());
+                        existingUser.setEducation((newStudent.getEducation()));
+                        repository.save(existingUser);
+                        return existingUser;
+                    })
+                    .get();
+            var entityModel = assembler.toModel(p);
+            return new ResponseEntity<>(entityModel, HttpStatus.OK);
+        } else {
+            log.info("ELSE!");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PatchMapping("/{id}")
