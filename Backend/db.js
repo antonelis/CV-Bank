50241f857fed8ecd78c4cbfa77ae20a0c81a7283
Puttzon <puttzon@gmail.com>
2020-09-22 08:16:22 +0200

    lagt in lite mer kod för koppling

--- Backend/db.js
+++ Backend/db.js
@@ -1,8 +1,32 @@
+// const nodeFetch = require('node-fetch')
 
 const express = require('express')
 const cors = require('cors')
-// const nodeFetch = require('node-fetch')
+const mysql = require('mysql')
+
+// Databas
+let connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: ''
+})
+connection.connect()
 
 const app = express()
-
 app.use(cors())
+
+//Get, Post etc.
+app.get('/', (req, res) => {
+  //Kod kommer
+})
+
+app.post('/', (req, res) => {
+  //Kod kommer
+})
+
+
+
+
+
+connection.end()
