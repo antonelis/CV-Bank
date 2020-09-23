f84f01434cb4b612c61d3ce52dae58d70e5dd9ee
puttzon <puttzon@gmail.com>
2020-09-23 12:47:24 +0200

    kommenterat bort dbjs

--- Frontend/cv-bank/src/components/CvLista.vue
+++ Frontend/cv-bank/src/components/CvLista.vue
@@ -18,93 +18,10 @@
             </button>
           </h2>
         </div>
-
         <div
           id="collapseOne"
           class="collapse show"
           aria-labelledby="headingOne"
-          data-parent="#accordionExample"
-        >
-          <div class="card-body">
-            <table class="table">
-              <thead>
-                <tr>
-                  <th scope="col">Namn:</th>
-                  <th scope="col">CV</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Kalle</td>
-                  <td>
-                    <a href="#">Öppna</a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-header" id="headingTwo">
-          <h2 class="mb-0">
-            <button
-              class="btn btn-block text-left collapsed"
-              type="button"
-              data-toggle="collapse"
-              data-target="#collapseTwo"
-              aria-expanded="false"
-              aria-controls="collapseTwo"
-            >
-              <h5>Webbutvecklare</h5>
-            </button>
-          </h2>
-        </div>
-        <div
-          id="collapseTwo"
-          class="collapse"
-          aria-labelledby="headingTwo"
-          data-parent="#accordionExample"
-        >
-          <div class="card-body">
-            <table class="table">
-              <thead>
-                <tr>
-                  <th scope="col">Namn:</th>
-                  <th scope="col">CV</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Kalle</td>
-                  <td>
-                    <a href="#">Öppna</a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-header" id="headingThree">
-          <h2 class="mb-0">
-            <button
-              class="btn btn-block text-left collapsed"
-              type="button"
-              data-toggle="collapse"
-              data-target="#collapseThree"
-              aria-expanded="false"
-              aria-controls="collapseThree"
-            >
-              <h5>Apputvecklare</h5>
-            </button>
-          </h2>
-        </div>
-        <div
-          id="collapseThree"
-          class="collapse"
-          aria-labelledby="headingThree"
           data-parent="#accordionExample"
         >
           <div class="card-body">
@@ -138,10 +55,9 @@ export default {
 </script>
 
 <style scoped>
-@media (max-width: 580px){
-  .CVdataBasBox{
-   padding: 0 15px; 
+@media (max-width: 580px) {
+  .CVdataBasBox {
+    padding: 0 15px;
   }
 }
-
 </style>
